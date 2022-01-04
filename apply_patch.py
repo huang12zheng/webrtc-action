7328310cc6f82cf3403a4f0551df3d474b53b84a
huang12zheng <805104533@qq.com>
2022-01-04 21:48:05 +0800

    feat: build_with_component

--- apply_patch.py
+++ apply_patch.py
@@ -51,5 +51,11 @@ def apply_patch(patch, dir='webrtc-build', depth=1):
                 cmd(['patch',f'-p{depth}'], stdin=stdin)
 
 if __name__ == '__main__':
-    for patch in ['clone_depot_tools_use_d1','container_preparations_advance','translation_into_english','fix_window_scriptinvocationpath_error']:
+    for patch in [
+        'clone_depot_tools_use_d1',
+        'container_preparations_advance',
+        'translation_into_english',
+        'fix_window_scriptinvocationpath_error',
+        'build_with_component'
+    ]:
         apply_patch(f'../{patch}.patch')
--- script.md
+++ script.md
@@ -3,6 +3,7 @@ cd webrtc-build
 git diff --cached > ../clone_depot_tools_use_d1.patch
 git diff --cached > ../translation_into_english.patch
 git diff --cached > ../container_preparations_advance.patch
+git diff --cached > ../build_with_component.patch
 ```
 
 ```sh
